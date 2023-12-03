1496549139869cbdd6cb66fef7b21c164f7f4f3a
Jaroslav Rusanov <sco.nuff@gmail.com>
2023-12-03 16:26:01 +0300

    assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,9 @@
+import { strict as assert} from 'node:assert'
+
 import capitalize from "../src/capitalize.js";
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает не Верно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает не Верно!');
-}
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
